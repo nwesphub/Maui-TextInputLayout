b69e03992d8cb38d2a09722b361244186f2e3316
nwesplan <56846321+nwesplan@users.noreply.github.com>
2025-02-21 17:05:11 -0600

    clean up

--- Maui.Android.TextInputLayout/Platforms/Android/MauiTextInputLayout.cs
+++ Maui.Android.TextInputLayout/Platforms/Android/MauiTextInputLayout.cs
@@ -1,6 +1,7 @@
 ﻿using Android.Content;
 using Android.Views;
 using Android.Widget;
+using AndroidX.AppCompat.Widget;
 using AndroidX.CoordinatorLayout.Widget;
 using Google.Android.Material.TextField;
 using System;
@@ -13,28 +14,12 @@ namespace Maui.Android.TextInputLayout.Platforms.Android
 {
     public class MauiTextInputLayout : Google.Android.Material.TextField.TextInputLayout
     {
+        public TextInputEditText _editText;
         public MauiTextInputLayout(Context context, ITextInputLayout layout) : base(context)
         {
-            RelativeLayout relativeLayout = new RelativeLayout(context)
-            {
-                LayoutParameters = new CoordinatorLayout.LayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent)
-                {
-                    Gravity = (int)GravityFlags.Center
-                }
-            };
-
-            // Create a VideoView and position it in the RelativeLayout
-            Google.Android.Material.TextField.TextInputLayout view = new Google.Android.Material.TextField.TextInputLayout(context)
-            {
-                LayoutParameters = new RelativeLayout.LayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent)
-            };
-
-            LayoutInflater inflater = (LayoutInflater)Context.GetSystemService(Context.LayoutInflaterService);
-            var view2 = inflater.Inflate(Resource.Layout.maui_text_input_layout, null, true);
-            this.AddView(view2);
-            // Add to the layouts
-            relativeLayout.AddView(view);
-            AddView(relativeLayout);
+            _editText = new TextInputEditText(context);
+            
+            this.AddView(_editText);
         }
     }
 }
