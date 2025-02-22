0fc17dfb933d386f4f97ad751fde01d3b04b3eed
nwesplan <56846321+nwesplan@users.noreply.github.com>
2025-02-21 20:46:01 -0600

    Text input edit text is now selectable from the entire width of its parent

--- Maui.Android.TextInputLayout/Platforms/Android/MauiTextInputEditText.cs
+++ Maui.Android.TextInputLayout/Platforms/Android/MauiTextInputEditText.cs
@@ -13,7 +13,12 @@ namespace Maui.Android.TextInputLayout.Platforms.Android
     {
         public MauiTextInputEditText(Context context) : base(context)
         {
-            LayoutParameters = new LLayout.LayoutParams(LinearLayout.LayoutParams.WrapContent, LinearLayout.LayoutParams.MatchParent);
+            SetWidth(50000);
+
+            // Set minimum width to a high number otherwise the input takes up a very small space and is not clickable
+            this.SetMinimumWidth(1000000);
+            LayoutParameters = new LLayout.LayoutParams(LinearLayout.LayoutParams.WrapContent, LinearLayout.LayoutParams.WrapContent);
+            
             SetMaxLines(1);
             SetSingleLine(true);
         }
