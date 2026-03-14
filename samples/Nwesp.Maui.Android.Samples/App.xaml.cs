e74b7193e72a62aaa01c8257ad703214ca23882b
nwesplan <56846321+nwesplan@users.noreply.github.com>
2026-03-14 16:39:26 -0500

    sample project

---
+++ samples/Nwesp.Maui.Android.Samples/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Nwesp.Maui.Android.Samples
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ samples/Nwesp.Maui.Android.Samples/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Nwesp.Maui.Android.Samples
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
