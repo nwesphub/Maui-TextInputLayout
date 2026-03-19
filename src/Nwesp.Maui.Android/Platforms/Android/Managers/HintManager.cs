51ca122da6a55ab9d00a0569ee71c40870b8f65a
nwesphub <56846321+nwesplan@users.noreply.github.com>
2026-03-19 18:28:45 -0500

    Hacky fix for the hint causing Filled mode layout to increase in height when focused

--- src/Nwesp.Maui.Android/Platforms/Android/Managers/HintManager.cs
+++ src/Nwesp.Maui.Android/Platforms/Android/Managers/HintManager.cs
@@ -42,7 +42,14 @@ namespace Nwesp.Maui.Android.Platforms.Android.Managers
 
         public static void MapIsHintAnimated(this MauiTextInputLayout platformView, ITextInputLayout virtualView)
         {
-            platformView.ExpandedHintEnabled = virtualView.IsHintAnimated;
+            
+            // Hack. Fix for Filled mode. When IsHintAnimated is set to true and the edit text does not have text, focusing the entry adds height to the layout which causes other elements in the layout to shift.
+            platformView.Post(async () =>
+            {
+                platformView.ExpandedHintEnabled = false;
+                await Task.Yield();
+                platformView.ExpandedHintEnabled = virtualView.IsHintAnimated;
+            });
         }
     }
 }
