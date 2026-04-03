199140f1bd4d154e96a6dd874215205347d3e39b
nwesphub <56846321+nwesplan@users.noreply.github.com>
2026-04-02 21:14:39 -0500

    Password Transformation - not implemented

--- src/Nwesp.Maui.Android/Platforms/Android/MaterialEditTextExtensions.cs
+++ src/Nwesp.Maui.Android/Platforms/Android/MaterialEditTextExtensions.cs
@@ -1,16 +1,22 @@
 ﻿using Android.Content.Res;
+using Android.Graphics;
+using Android.Text;
+using Android.Text.Method;
+using Android.Text.Style;
 using Android.Widget;
-using Nwesp.Maui.Android.Utilities;
+using Java.Lang;
+using Microsoft.Maui;
 using Microsoft.Maui.Platform;
+using Nwesp.Maui.Android.Abstractions;
+using Nwesp.Maui.Android.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AResource = Android.Resource.Attribute;
-using Nwesp.Maui.Android.Abstractions;
-using Android.Text;
-using Microsoft.Maui;
+using AView = Android.Views.View;
+using APaint = Android.Graphics.Paint;
 namespace Nwesp.Maui.Android.Platforms.Android
 {
     public static class MaterialEditTextExtensions
@@ -31,7 +37,7 @@ namespace Nwesp.Maui.Android.Platforms.Android
 
             editText.SetTextColor(new ColorStateList(states, colors));
         }
-
+        
         public static void TogglePasswordOn(this EditText editText)
         {
             if (editText.InputType.HasFlag(InputTypes.ClassText))
@@ -46,6 +52,79 @@ namespace Nwesp.Maui.Android.Platforms.Android
                 editText.InputType &= ~InputTypes.TextVariationPassword;
             if (editText.InputType.HasFlag(InputTypes.ClassNumber))
                 editText.InputType &= ~InputTypes.NumberVariationPassword;
+        }
+
+        //public static void TogglePasswordOn(this EditText editText)
+        //{
+        //    editText.TransformationMethod = new SpacedPasswordTransformationMethod("• ");
+        //    editText.SetSelection(editText.Text?.Length ?? 0);
+        //}
+
+        //public static void TogglePasswordOff(this EditText editText)
+        //{
+        //    editText.TransformationMethod = null;
+        //    editText.SetSelection(editText.Text?.Length ?? 0);
+        //}
+        public class SpacedPasswordTransformationMethod : PasswordTransformationMethod
+        {
+            private readonly string _mask;
+
+            public SpacedPasswordTransformationMethod(string mask = "• ")
+            {
+                _mask = mask;
+            }
+
+            public override ICharSequence? GetTransformationFormatted(ICharSequence? source, AView? view)
+            {
+                if (source == null)
+                    return null;
+
+                var spannable = new SpannableString(source);
+
+                for (int i = 0; i < source.Length(); i++)
+                {
+                    spannable.SetSpan(new DotSpan(), i, i + 1, SpanTypes.ExclusiveExclusive);
+                }
+
+                return spannable;
+            }
+
+            private class DotSpan : ReplacementSpan
+            {
+
+                
+                private const float SpacingDp = 4f;     // horizontal spacing
+                private const float Scale = 1.4f;      // 🔥 increase dot size (1.0 = normal)
+
+                public override int GetSize(APaint paint, ICharSequence? text, int start, int end, APaint.FontMetricsInt? fm)
+                {
+                    var originalSize = paint.TextSize;
+                    paint.TextSize = originalSize * Scale;
+
+                    float width = paint.MeasureText("•");
+
+                    float spacingPx = SpacingDp * 2.75f;
+
+                    paint.TextSize = originalSize;
+
+                    return (int)(width + spacingPx);
+                }
+
+                public override void Draw(Canvas canvas, ICharSequence? text, int start, int end,
+    float x, int top, int y, int bottom, APaint paint)
+                {
+                    var originalSize = paint.TextSize;
+
+                    float scaledSize = originalSize * Scale;
+                    float offset = (scaledSize - originalSize) / 2f; // 👈 push DOWN
+
+                    paint.TextSize = scaledSize;
+
+                    canvas.DrawText("•", x, y + offset, paint);
+                    // • ●
+                    paint.TextSize = originalSize;
+                }
+            }
         }
     }
 }
